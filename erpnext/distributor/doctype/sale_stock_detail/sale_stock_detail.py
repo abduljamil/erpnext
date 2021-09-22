b010ea258ead76dfbccbd36b6cb00f807bed89ee
UsamaNaveed9 <usamanaveed9263@gmail.com>
2021-09-22 11:59:03 +0500

    Feat:Add Code for extracting pdf data of Mainwali distributor

--- erpnext/distributor/doctype/sale_stock_detail/sale_stock_detail.py
+++ erpnext/distributor/doctype/sale_stock_detail/sale_stock_detail.py
@@ -333,7 +333,7 @@ def parse_pdf(pdf_file,dist_city):
                 name = require_data[x][-1] +" "+ require_data[x][-2] +" "+ require_data[x][-3]
                 require_data[x].append(name)
         info = filter_data_timergrah(require_data)
-    elif(dist_city=="Bhakkar"): 
+    elif(dist_city=="Bhakkar" or dist_city=="Mainwali"): 
         with pdfplumber.open(path) as pdf:
             for x in range(0, len(pdf.pages)):
                 data = pdf.pages[x].extract_text()
@@ -344,17 +344,24 @@ def parse_pdf(pdf_file,dist_city):
                     arr = list(filter(None, arr))
                     require_data.append(arr) 
             for x in require_data[:]:
-                for y in range(0,len(x)):  
-                    if(x[y]=='-'):
-                        x[y]="0"
-                x[0] = re.sub(r'[0-9]',"",x[0])
-                if(len(x)<14 or x[0]=="PR.ID"):
-                    require_data.remove(x)       
+                if(dist_city=="Bhakkar"):
+                    for y in range(0,len(x)):  
+                        if(x[y]=='-'):
+                            x[y]="0"
+                    x[0] = re.sub(r'[0-9]',"",x[0])
+                    if(len(x)<14 or x[0]=="PR.ID"):
+                        require_data.remove(x)
+                if(dist_city=="Mainwali"):
+                    if(len(x)<11 or x[0]=="Name"):
+                        require_data.remove(x)               
             for x in range(0,len(require_data)):
                 require_data[x].reverse()
                 name = require_data[x][-1] +" "+ require_data[x][-2]
                 require_data[x].append(name)
-        info = filter_data_bhakkar(require_data)
+        if(dist_city=="Bhakkar"):            
+            info = filter_data_bhakkar(require_data)
+        elif(dist_city=="Mainwali"):
+            info = filter_data_mainwali(require_data)
     elif(dist_city=="Faisalabad" ):
         with pdfplumber.open(path) as pdf:
             for x in range(0, len(pdf.pages)):
@@ -931,3 +938,27 @@ def filter_data_gujrat(require_data): #for gujrat
         filter_data_copy = filter_data.copy()
         final_data.append(filter_data_copy)    
     return final_data
+
+@frappe.whitelist(allow_guest=True)
+def filter_data_mainwali(require_data): #for mainwali
+    filter_data = {}
+    final_data = []
+    index_arr = [-1,7,6,5,3,2] #[item, opening balance, purchase,return,sale,bonus]
+    #get data with specific index
+    for x in require_data:
+        for i in index_arr:
+            if i == -1:
+                filter_data['item'] = x[i]   
+            elif i == 7:
+                filter_data['opening_stock'] = x[i]
+            elif i == 6:
+                filter_data['purchase'] = x[i]
+            elif i == 5:
+                filter_data['return'] = x[i]     
+            elif i == 3:
+                filter_data['sale'] = x[i]
+            elif i == 2:
+                filter_data['bonus'] = x[i]      
+        filter_data_copy = filter_data.copy()
+        final_data.append(filter_data_copy)    
+    return final_data   
