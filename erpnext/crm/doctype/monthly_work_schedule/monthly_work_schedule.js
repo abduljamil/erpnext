0d8a2ccf4bccb5bafc36de9d0a92cf27bd7b12e6
ArslanAli394 <68888409+ArslanAli394@users.noreply.github.com>
2021-10-23 11:36:02 +0500

    Feat: Update the monthly work schedule
    In monthly work schedule, it will auto show all the days in child doctype in between the from and to date.

--- erpnext/crm/doctype/monthly_work_schedule/monthly_work_schedule.js
+++ erpnext/crm/doctype/monthly_work_schedule/monthly_work_schedule.js
@@ -1,28 +1,48 @@
 // Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
-
+function addDays(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
 frappe.ui.form.on('Monthly Work Schedule', {
 	from_date: function(frm) {
+		cur_frm.clear_table("work_schedule_detail");
 			// for get last date
-	var depreciation_start_date = moment(frm.doc.from_date).endOf('month').format('DD-MM-YYYY');
-	frm.set_value('to_date',depreciation_start_date);
-	
-	// for get day of selected month
-	// var a = new Date(cur_frm.doc.from_date);
-	// var child = cur_frm.add_child("work_schedule_detail");
-
-
-	// if( (a.getHours()>= 8 && a.getMinutes() >= 30) && (a.getHours()<= 10 && a.getMinutes() <= 30) ){
-	// 	// cur_frm.set_value("session","First Session")
-	// 	frappe.model.set_value(child.doctype, child.name, "day", "First Session")
-	// } else if ( a.getHours()>= 11 && a.getHours()<= 14 ){
-	// 	// cur_frm.set_value("session","Second Session")
-	// 	frappe.model.set_value(child.doctype, child.name, "day", "Second Session")
-		
-	// } else if ( a.getHours()>= 15 && a.getHours()<= 17  ){
-	// 	// cur_frm.set_value("session","Third Session")
-	// 	frappe.model.set_value(child.doctype, child.name, "day", "Third Session")
-	// }
+		var depreciation_start_date = moment(frm.doc.from_date).endOf('month').format('YYYY-MM-DD');
+		frm.set_value('to_date',depreciation_start_date);
+		//check to date exists
+		if(frm.doc.to_date){
+			//get date array which contain all the date between the from and to date
+			var dateArray = new Array();
+			var stopDate = new Date(frm.doc.to_date).getDate();
+			var currentDate = new Date(frm.doc.from_date).getDate();
+			while (currentDate <= stopDate) {
+				var tomorrow = new Date();
+				tomorrow.setDate(currentDate);
+				dateArray.push(tomorrow);
+				currentDate = currentDate+1
+			}
+			if(dateArray!=[]){
+				//map the date array 
+				for (let index = 0; index < dateArray.length; index++) {
+					let element = dateArray[index];
+					//get day by date
+					var a = new Date(element);
+					if(a.getDay()!==0){
+						var weekDays = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+						var child = cur_frm.add_child("work_schedule_detail");
+						frappe.model.set_value(child.doctype, child.name, "day", weekDays[a.getDay()]);
+						// check if area is enter then also default set the area town 
+						if(frm.doc.area){
+							frappe.model.set_value(child.doctype, child.name, "areatown", frm.doc.area);
+						}
+						cur_frm.refresh_field("work_schedule_detail")
+					}
+					
+				}
+			}
+		}
 	
 	}
 });
